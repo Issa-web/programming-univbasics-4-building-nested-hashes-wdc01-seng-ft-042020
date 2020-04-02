f4f20959ff407ccd35741a1e96ebe49a75755667
Issa Sangare <issabmsangare@hotmail.com>
2020-04-02 00:22:43 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -4,7 +4,7 @@ def base_hash
 end
 
 def monopoly_with_second_tier
- {:railroads => {:pieces => a}, :rent_in_dollars => {}, :name => {}}  
+ {:railroads => {:pieces => 4}, :rent_in_dollars => {}, :name => {}}  
 
 end
 
